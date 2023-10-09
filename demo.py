5cb52333470b040b051ddf5cc5f8cab401f690a0
Tanushri Nayak <tanushri0310003@gmail.com>
2023-10-09 15:59:17 +0200

    Solved demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,10 @@
 """Dummy challenge for Kitt Demo"""
-
+import math
 
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    if radius < 0:
+        return 0
+    return radius * radius * math.pi
+    #pass  # YOUR CODE HERE
